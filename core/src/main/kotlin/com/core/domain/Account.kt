cd71aa596d08f64982eabb25738b0d5db0a70e4a
rubens <rubens.yamasaki@codeitsolutions.com.br>
2022-06-04 19:31:54 -0300

    Creating new modules

---
+++ core/src/main/kotlin/com/core/domain/Account.kt
@@ -0,0 +1,23 @@
+package com.core.domain
+
+class Account(
+        val id: Long,
+        val ownerName: String,
+        val ownerDocument: String,
+        var balance: Money
+        
+) {
+    
+    fun witdhdraw(money: Money): Boolean {
+        if (balance isLessThan  money) {
+            return false
+        }
+        balance = this.balance subtract money
+        return true
+    }
+    
+    fun deposit(money: Money) {
+        balance = this.balance sum money
+    }
+    
+}
