f6609f637087b43a236cbbedd27e5650a14eb14c
rubens <rubens.yamasaki@codeitsolutions.com.br>
2022-06-04 21:15:41 -0300

    Creating new test

--- core/src/test/kotlin/com/core/domain/AccountTest.kt
+++ core/src/test/kotlin/com/core/domain/AccountTest.kt
@@ -24,6 +24,16 @@ class AccountTest {
         Assertions.assertEquals(BigInteger.valueOf(1000), account1000Balance.getAccountBalance())
     }
     
+    @Test
+    fun shouldDepositMoney() {
+        val account1000Balance = createAccountWith100OfBalance()
+        val money = Money(BigInteger.valueOf(150))
+        
+        account1000Balance.deposit(money)
+        Assertions.assertEquals(BigInteger.valueOf(1150), account1000Balance.getAccountBalance())
+        
+    }
+    
     private fun createAccountWith100OfBalance() = 
             Account(id = 100,
                     ownerName = "Jose Teste",
